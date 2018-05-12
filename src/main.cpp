bdf8d6dd511a30e80cf2c7aa3a489b7c9c143bad
sergeil <vectra14@gmail.com>
2018-05-12 12:13:09 +0200

    minor: update default scenario name

--- src/main.cpp
+++ src/main.cpp
@@ -38,7 +38,7 @@ int randomNumCarry=-1;
 void OnTimer(int v);
 
 vector<QuadcopterHandle> CreateVehicles();
-string _scenarioFile="../config/1_Intro.txt";
+string _scenarioFile="../config/01_Intro.txt";
 
 #include "MavlinkNode/MavlinkNode.h"
 shared_ptr<MavlinkNode> mlNode;
